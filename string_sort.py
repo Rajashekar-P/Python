462f993e649d352c012012292fa628b5cb7e5865
Rajashekar <35585996+Rajashekar-P@users.noreply.github.com>
2020-05-13 16:51:35 +0530

    Created string_sort.py
    A Simple python program to sort a string using sort function

---
+++ string_sort.py
@@ -0,0 +1,14 @@
+    string = input("Enter a string: ")  
+    
+    # breakdown the string into a list of words  
+    
+    words = string.split()  
+    
+    # sort the list  
+    
+    words.sort()  
+    
+    # display the sorted words  
+    
+    for word in words:  
+       print(word)  
