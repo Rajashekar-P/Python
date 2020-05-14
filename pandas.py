7fb0e8456ff4f967696c4c731f98f288dbe5c688
Rajashekar <35585996+Rajashekar-P@users.noreply.github.com>
2020-05-14 18:17:25 +0530

    Created pandas.py

---
+++ pandas.py
@@ -0,0 +1,12 @@
+//First import pandas and read csv file using pandas tool
+
+import pandas as pd
+
+// i created csv file with some data and given as name sample to that csv file
+// read_csv is in built function of pandas it will read the csv data.
+
+data = pd.read_csv("sample.csv")
+
+//by running print command we will get data from csv file.
+
+print(data)
